c3338193ea9b62a7edffc7a5508cac38c00d3b13
abastiandwi <abastian.dwi@dwidasa.com>
2024-05-18 16:38:13 +0700

    soal_satu_development: create loop algorithm sesuai dengan soal nomor 1

---
+++ src/main/java/abastian/dwi/saputra/testtechnicaldevelopment/soalsatudevelopment/SoalSatuDevelopmentApp.java
@@ -0,0 +1,19 @@
+package abastian.dwi.saputra.testtechnicaldevelopment.soalsatudevelopment;
+
+public class SoalSatuDevelopmentApp {
+    public static void main(String[] args) {
+        /*
+            Soal Test 1 Development
+            LOOP ALGORITHM
+            By: Abastian Dwi Saputra
+         */
+        int numberLength = 5;
+
+        for (int i = 1; i <= numberLength; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
