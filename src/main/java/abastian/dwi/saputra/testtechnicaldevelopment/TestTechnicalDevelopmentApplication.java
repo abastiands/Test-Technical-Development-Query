2b7c6f572346cefe10b12315de5d3a75de6af9d0
abastiandwi <abastian.dwi@dwidasa.com>
2024-05-18 16:27:18 +0700

    main: init app test technical-development

---
+++ src/main/java/abastian/dwi/saputra/testtechnicaldevelopment/TestTechnicalDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package abastian.dwi.saputra.testtechnicaldevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTechnicalDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTechnicalDevelopmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/abastian/dwi/saputra/testtechnicaldevelopment/TestTechnicalDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package abastian.dwi.saputra.testtechnicaldevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTechnicalDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
